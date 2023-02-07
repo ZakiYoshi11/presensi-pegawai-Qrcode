85228840632d54058f288a047f6c38b4f85e83ad
Zaki Rusydi <98293373+ZakiYoshi11@users.noreply.github.com>
2023-02-07 22:30:58 +0700

    Create config.php

---
+++ admin/config.php
@@ -0,0 +1,7 @@
+<?php
+$host       = "localhost";
+$user       = "root";
+$password   = "";
+$database   = "db_presensi_puskesmas";
+$connect    = mysqli_connect($host, $user, $password, $database)
+?>
